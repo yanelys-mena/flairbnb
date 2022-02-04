94a98f7c113622d8ca40c7826fe07757fc4f51da
yanelys-mena <yanelyshm@outlook.com>
2022-02-03 21:03:55 -0500

    csrfSetup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,19 @@ import './index.css';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { restoreCSRF, csrfFetch } from './store/csrf';
+
 import App from './App';
 import configureStore from './store';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
-  window.store = store;
-};
+  restoreCSRF();
 
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
 function Root() {
   return (
     <Provider store={store}>

d7f3f5487b458829d5831ca5710995650c870b83
yanelys-mena <yanelyshm@outlook.com>
2022-02-07 12:32:50 -0500

    test

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -35,41 +35,45 @@ function LoginForm() {
 
     return (
         <>
-            <div className="Welcome">
-                <h2> Welcome to Flairbnb </h2>
-            </div>
-            <form
-                className="loginForm"
-                onSubmit={handleSubmit} >
-                <ul>
-                    {errors.map((error, idx) => (
-                        <li key={idx}>{error}</li>
-                    ))}
-                </ul>
-                <label>
-                    {/* Email */}
-                    <input
-                        type="text"
-                        value={email}
-                        placeholder="Email"
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                </label>
-                <label>
-                    {/* Password */}
-                    <input
-                        type="password"
-                        value={password}
-                        placeholder="Password"
+            <div className="formModal">
 
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </label>
-                <button type="submit">Log In</button>
-                <button type="submit" onClick={handleDemoLogin}>Demo Login</button>
-            </form>
+
+                <div className="Welcome">
+                    <h2> Welcome to Flairbnb </h2>
+                </div>
+                <form
+                    className="loginForm"
+                    onSubmit={handleSubmit} >
+                    <ul>
+                        {errors.map((error, idx) => (
+                            <li key={idx}>{error}</li>
+                        ))}
+                    </ul>
+                    <label>
+                        {/* Email */}
+                        <input
+                            type="text"
+                            value={email}
+                            placeholder="Email"
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <label>
+                        {/* Password */}
+                        <input
+                            type="password"
+                            value={password}
+                            placeholder="Password"
+
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <button type="submit">Log In</button>
+                    <button type="submit" onClick={handleDemoLogin}>Demo Login</button>
+                </form>
+            </div>
         </>
     );
 }

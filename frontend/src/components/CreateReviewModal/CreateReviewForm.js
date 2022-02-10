bf9c72de087a352c0c015d5fca39b71fe07f484f
yanelys-mena <yanelyshm@outlook.com>
2022-02-10 15:50:18 -0500

    CREATE REVIEW FORM FULLY WORKING

--- frontend/src/components/CreateReviewModal/CreateReviewForm.js
+++ frontend/src/components/CreateReviewModal/CreateReviewForm.js
@@ -7,10 +7,11 @@ import './CreateReview.css';
 
 function CreateReviewForm() {
     const dispatch = useDispatch();
-    const [rating, setRating] = useState("");
+    const [rating, setRating] = useState(null)
+    const [hover, setHover] = useState(null)
     const [userReview, setUserReview] = useState("");
     const [errors, setErrors] = useState([]);
-
+    console.log(rating)
 
 
     const handleSubmitReview = (e) => {
@@ -38,33 +39,45 @@ function CreateReviewForm() {
                         <li key={idx}>{error}</li>
                     ))}
                 </ul>
-                <label>
-                    {/* Rating */}
-                    <input type="radio" name="rating" id="ratingRadioButton"></input>
-                    {[...Array(5)].map((star) => (
-                        <FaStar />
-                    ))}
 
-                    {/* <input
-                        id="ratingInput"
-                        type="text"
-                        value={rating}
-                        placeholder="rating"
-                        onChange={(e) => setRating(e.target.value)}
-                        required
-                    /> */}
-                </label>
-                <label>
-                    {/* Review */}
-                    <input
-                        id="reviewInput"
-                        type="text"
-                        value={userReview}
-                        placeholder="review"
-                        onChange={(e) => setUserReview(e.target.value)}
-                        required
-                    />
-                </label>
+                <div id="starRating">
+
+                    {[...Array(5)].map((star, idx) => {
+                        const ratingVal = idx + 1;
+                        return (
+                            <label key={idx}>
+                                <input
+                                    type="radio"
+                                    name="rating"
+                                    value={ratingVal}
+                                    onClick={() => setRating(ratingVal)}
+                                />
+                                <FaStar
+                                    className="ratingStars"
+                                    size={40}
+                                    onMouseEnter={() => setHover(ratingVal)}
+                                    onMouseLeave={() => setHover(null)}
+                                    color={ratingVal <= (hover || rating) ? "FFA534" : "#e4e5e9"} />
+                            </label>
+                        )
+                    })}
+
+
+                </div>
+
+                <div>
+                    <label>
+                        {/* Review */}
+                        <input
+                            id="reviewInput"
+                            type="text"
+                            value={userReview}
+                            placeholder="review"
+                            onChange={(e) => setUserReview(e.target.value)}
+                            required
+                        />
+                    </label>
+                </div>
                 <button type="submit">Submit</button>
             </form>
         </>

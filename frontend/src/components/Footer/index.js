34fc5bfba2d6b05d22547deea7fc1f229ece2c0e
yanelys-mena <yanelyshm@outlook.com>
2022-02-10 01:37:32 -0500

    footer detail

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -5,7 +5,15 @@ const Footer = () => {
         <div className="footer">
 
             <div className="bottomFooter">
-                © 2022 Flairbnb · Yanelys Mena  ·  <a href="https://github.com/yanelys-mena/flairbnb#readme" target="blank">Read Me</a> · <a href="https://github.com/yanelys-mena" target="blank">GitHub</a> ·  <a href="https://www.linkedin.com/in/yanelysmena/" target="blank"><i class="fa-brands fa-linkedin-in"></i></a>
+                <span> © 2022 Flairbnb</span>
+                <span> · </span>
+                <span>Yanelys Mena</span>
+                <span> · </span>
+                <a href="https://github.com/yanelys-mena/flairbnb#readme" target="blank">Read Me</a>
+                <span> · </span>
+                <a href="https://github.com/yanelys-mena" target="blank">GitHub </a>
+                <span> · </span>
+                <a href="https://www.linkedin.com/in/yanelysmena/" target="blank"> <i class="fab fa-linkedin"></i></a>
             </div>
         </div >
     )

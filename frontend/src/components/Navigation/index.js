c320eecf1fe844f6bf6e30e8493d9b71d29a6d80
yanelys-mena <yanelyshm@outlook.com>
2022-02-10 23:12:25 -0500

    alignment safari login/signup

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -42,35 +42,4 @@ function Navigation({ isLoaded }) {
 }
 
 
-// function Navigation({ isLoaded }) {
-//     const sessionUser = useSelector(state => state.session.user);
-
-//     let sessionLinks;
-//     if (sessionUser) {
-//         sessionLinks = (
-//             <div className="profile_btn">  <li> <ProfileButton user={sessionUser} /></li></div>
-//         );
-//     } else {
-//         sessionLinks = (
-//             <>
-//                 <li>  <LoginFormModal /></li>
-//                 <li>  <NavLink to="/login">Log In</NavLink></li>
-//                 <li><NavLink to="/signup">Sign Up</NavLink></li>
-//             </>
-//         );
-//     }
-
-//     return (
-//         <nav>
-//             <ul className="navBar">
-//                 <li>
-//                     <NavLink exact to="/">Home</NavLink>
-//                 </li>
-//                 {isLoaded && sessionLinks}
-
-//             </ul>
-//         </nav>
-//     );
-// }
-
 export default Navigation;

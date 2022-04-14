9cea125850df2b42569d42a90a2939d4397099c8
yanelys-mena <yanelyshm@outlook.com>
2022-04-14 15:45:26 -0400

    info rendered correctly

--- backend/db/seeders/20220414004522-bookingseeds.js
+++ backend/db/seeders/20220414004522-bookingseeds.js
@@ -6,8 +6,8 @@ module.exports = {
     return queryInterface.bulkInsert('Bookings', [
       { userId: 1, listingId: 1, startDate: '2022-04-17', endDate: '2022-04-20', numGuests: 2 },
       { userId: 1, listingId: 2, startDate: '2022-04-25', endDate: '2022-05-01', numGuests: 1 },
-      { userId: 2, listingId: 22, startDate: '2022-04-25', endDate: '2022-05-01', numGuests: 1 },
-      { userId: 3, listingId: 22, startDate: '2022-04-17', endDate: '2022-04-20', numGuests: 1 },
+      { userId: 2, listingId: 18, startDate: '2022-04-25', endDate: '2022-05-01', numGuests: 1 },
+      { userId: 3, listingId: 18, startDate: '2022-04-17', endDate: '2022-04-20', numGuests: 1 },
     ], {});
   },
 

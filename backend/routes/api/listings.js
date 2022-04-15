197353df28ab104ebaef92792bc086f9addc8984
yanelys-mena <yanelyshm@outlook.com>
2022-04-15 17:05:32 -0400

    including my listing Booking

--- backend/routes/api/listings.js
+++ backend/routes/api/listings.js
@@ -6,7 +6,8 @@ const { handleValidationErrors } = require('../../utils/validation');
 const { Listing } = require('../../db/models');
 const { Image } = require('../../db/models');
 const { Review } = require('../../db/models');
-const { User } = require('../../db/models')
+const { User } = require('../../db/models');
+const { Booking } = require('../../db/models');
 
 const router = express.Router();
 
@@ -16,7 +17,7 @@ router.get(
     asyncHandler(async (req, res, next) => {
         const listings = await Listing.findAll({
             include: [
-                User, Image, Review
+                User, Image, Review, Booking
             ]
         });
         return res.json(listings)
--- frontend/src/components/SearchBar/index.js
+++ frontend/src/components/SearchBar/index.js
@@ -1,18 +1,28 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { addDays } from 'date-fns';
+import { getAllListings } from '../../store/listings';
+import { load_bookings } from '../../store/bookings';
+
 import './SearchBar.css';
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import { DateRangePicker } from 'react-date-range';
 import dayjs from "dayjs";
+import { useDispatch, useSelector } from 'react-redux';
 
 
 
 export default function SearchBar() {
     const history = useHistory();
-    const [showPicker, setShowPicker] = useState(false)
+    const listings = useSelector((state) => state?.listings);
+    const bookings = useSelector((state) => state?.bookings);
 
+    const dispatch = useDispatch();
+
+    const [showPicker, setShowPicker] = useState(false);
+    const [location, setLocation] = useState('');
+    const [guest, setGuest] = useState('');
     const [state, setState] = useState([
         {
             startDate: new Date(),
@@ -21,6 +31,12 @@ export default function SearchBar() {
         }
     ]);
 
+    useEffect(() => {
+        dispatch(getAllListings());
+        dispatch(load_bookings())
+
+    }, [dispatch])
+
 
     useEffect(() => {
         if (!(state[0].startDate.toISOString().slice(0, 10) === state[0].endDate.toISOString().slice(0, 10))) {
@@ -28,19 +44,33 @@ export default function SearchBar() {
         }
     }, [state]);
 
+    const handleSearch = (e) => {
+        // if location -> filter all listings.city || listings.state
+        // if guest => filter guests =< listing.guests
+        // if 
+        /*
+startDate.toISOString().slice(0, 10) >= item.startDate && startDate.toISOString().slice(0, 10) <= item.endDate
+                    || endDate.toISOString().slice(0, 10) >= item.startDate && endDate.toISOString().slice(0, 10) <= item.endDate
+        */
+    }
 
 
     return (
         <div id="searchBarDiv">
             <div id="searchComponent">
-                <input
-                    type='text'
-                    placeholder='Where are you going?'>
-                </input>
+                <div id="location_search">
+                    <input
+                        type='text'
+                        placeholder='Where are you going?'
+                        value={location}
+                        onChange={(e) => setLocation(e.target.value)}>
+                    </input>
+                </div>
+
                 <div id="search_check_parent">
                     <div id="search_start_date" onClick={() => setShowPicker(!showPicker)}>
                         <div id="check_in"> Check in</div>
-                        <div>{state[0].startDate && state[0].endDate ? <> {`${dayjs(state[0].startDate).format("MMM DD")} - ${dayjs(state[0].endDate).format("MMM DD")}`} </> : 'Add dates'}</div>
+                        <div id="search_date_range">{state[0].startDate && state[0].endDate ? <> {`${dayjs(state[0].startDate).format("MMM DD")} - ${dayjs(state[0].endDate).format("MMM DD")}`} </> : 'Add dates'}</div>
                     </div>
                     {showPicker &&
                         <div id="date_range_pop_up">
@@ -55,8 +85,17 @@ export default function SearchBar() {
                         </div>
                     }
                 </div>
-
-
+                <div id="guest_search">
+                    <input
+                        type='text'
+                        value={guest}
+                        onChange={(e) => setGuest(e.target.value)}
+                        placeholder='Where are you going?'>
+                    </input>
+                </div>
+                <div id="search_btn">
+                    <button onClick={handleSearch}> Search  </button>
+                </div>
             </div>
         </div >
     )
